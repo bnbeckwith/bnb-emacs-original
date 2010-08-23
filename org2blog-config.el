8c18ad977f63cccdd9829f70c669b3d770674385
Benjamin Beckwith <bnbeckwith@gmail.com>
2010-08-23 13:51:59 -0400

    Added global mode key

--- org2blog-config.el
+++ org2blog-config.el
@@ -5,6 +5,10 @@
 			 (concat bnb-elisp-dir "org2blog/")))
 (add-to-list 'load-path (expand-file-name
 			 (concat bnb-elisp-dir "xml-rpc-el/")))
+
+
+(global-set-key "\C-c2" 'org2blog-mode)
+
 (setq org2blog-server-url "http://bnbeckwith.com/xmlrpc.php"
       org2blog-server-user "Ben")
 
