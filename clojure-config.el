84dd53694c0165ebd1031d7abbac1f61c79c9c10
Benjamin Beckwith <bnbeckwith@gmail.com>
2011-08-17 11:53:08 -0400

    Added slime-config and removed swank-clojure from clojure-config.el

--- clojure-config.el
+++ clojure-config.el
@@ -1,6 +1,6 @@
+;; Clojure mode 
+
 (add-to-list 'load-path (list (expand-file-name
-			       (concat bnb-elisp-dir "swank-clojure"))
-			      (expand-file-name
 			       (concat bnb-elisp-dir "clojure-mode"))))
 
 (require 'clojure-mode)
