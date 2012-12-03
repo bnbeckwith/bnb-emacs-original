30c4767fe3334bf21caa9db4ef4606d74493bf0f
Benjamin Beckwith <benjamin.beckwith@intel.com>
2012-12-03 16:47:29 -0500

    Many updates for the emacs and org-version updates

--- org-config.el
+++ org-config.el
@@ -5,25 +5,19 @@
 ;;;;;
 ;; ORG INSTALLATION
 ;;;;;
-(setq bnb-org-location (concat bnb-elisp-dir "org-mode/"))
 
-;; Tell emacs where to find it and when to start it
-(add-to-list 'load-path (expand-file-name (concat bnb-org-location "lisp/")) 'prepend)
-(add-to-list 'load-path (expand-file-name (concat bnb-org-location "contrib/lisp")) 'prepend)
 	     ;; .org .org_archive and .org.gpg files are org-mode files
 (add-to-list 'auto-mode-alist '("\\.org\\(.gpg|_archive\\)?$" . org-mode))
 
 ; Install org-mode
-(require 'org-install)
+(require 'org)
 (require 'org-protocol)
 
 ;; setup some hooks.
 (add-hook 'org-mode-hook
 	  (lambda ()
 	    ;; Keybindings
-	    (local-set-key "\M-I" 'org-toggle-iimage-in-org)
-	    ;; Turn on flyspell mode
-					;(flyspell-mode 1)
+	    (local-set-key "\M-I" 'org-toggle-inline-images)
 	    ))
 (add-hook 'bnb/really-kill-emacs-hooks 'org-save-all-org-buffers 'append)
 
@@ -31,30 +25,18 @@
 (global-set-key (kbd "C-c C-t") 'orgtbl-mode)
 
 ;;;;;
-;; Overrides
-;;;;; 
-
-;; Use Intel Workweek
-(defadvice org-days-to-iso-week (around bnb/intel-workweek activate)
-  "Fix the ISO workweek to be Intel's idea of Workweek"
-  (setq ad-return-value
-	(bnb/workweek)))
-
-;;;;;
 ;; ORG MISCELLANEOUS
 ;;;;;
-
-;; Add the installed info directory to the Info list
-(add-to-list 'Info-default-directory-list (concat bnb-org-location "doc/org"))
 
 ;; Save all of my org-buffers every hour.
 (run-at-time "00:59" 3600 'org-save-all-org-buffers)
 
-;; Setup applications to use to open links.
-(setq org-file-apps 
-      (quote ((auto-mode . emacs) 
-	      ("\\.x?html?\\'" . default) 
-	      ("\\.pdf\\'" . default) 
+;; Setup applications to use to open links. This lets the sytem (OS)
+;; decide which application is necesary for the given filename.
+(setq org-file-apps
+      (quote ((auto-mode . emacs)
+	      ("\\.x?html?\\'" . default)
+	      ("\\.pdf\\'" . default)
 	      ("\\.mm\\'" . default))))
 
 ;; Use the same interface as REFILE when using org-goto
@@ -79,7 +61,7 @@
 ;; Hide the leading stars so that we aren't seeing stars.
 (setq org-hide-leading-stars t)
 
-;; Do NOT use odd-levels only (this is the default, but let's be
+;; Do NOT use odd-levels only (nil is the default, but let's be
 ;; explicit)
 (setq org-odd-levels-only nil)
 
@@ -98,14 +80,15 @@
 ;; Hidestars globally
 (setq org-hide-leading-stars t)
 
-;; Fontify the SRC blocks natively 
+;; Fontify the SRC blocks natively
 (setq org-src-fontify-natively t)
 
-;; Google Weather
-(add-to-list 'load-path (concat bnb-elisp-dir "google-weather-el/"))
-; I had to turn off https to have this work at work.
-(setq google-weather-use-https nil)
-(require 'org-google-weather)
+;; DELETE THIS SECTION
+;; ;; Google Weather
+;; (add-to-list 'load-path (concat bnb-elisp-dir "google-weather-el/"))
+;; ; I had to turn off https to have this work at work.
+;; (setq google-weather-use-https nil)
+;; (require 'org-google-weather)
 
 ;;;;
 ;; ORG KEYS
@@ -116,14 +99,13 @@
 (global-set-key "\C-cr" 'org-capture)
 (global-set-key "\C-cb" 'org-iswitchb)
 (global-set-key (kbd "<f12>") 'org-agenda)
-(global-set-key (kbd "<f9> I") 
-		'(lambda () (interactive) (info (concat bnb-org-location "doc/org"))))
 (global-set-key (kbd "<f9> v") 'visible-mode)
-(global-set-key (kbd "<f9> i") 'bnb/clock-in)
-(global-set-key (kbd "<f9> o") 'bnb/clock-out)
+(global-set-key (kbd "<f9> i") 'org-clock-in)
+(global-set-key (kbd "<f9> o") 'org-clock-out)
 (global-set-key (kbd "<f11> i") 'org-clock-in)
 (global-set-key (kbd "<f11> g") 'org-clock-goto)
-(global-set-key (kbd "<f5> s") 'flyspell-buffer)
+;; TODO -- this should be moved elsewhere.
+(global-set-key (kbd "<f11> s") 'flyspell-buffer)
 
 
 ;; Speed commands are used when on the * of a given headline.  If
@@ -146,14 +128,14 @@
 ;;;;
 
 ;; Setup the TODO keyword sequences.
-(setq org-todo-keywords 
+(setq org-todo-keywords
       (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
 	      (sequence "WAITING(w@/!)" "SOMEDAY(s!)" "|" "CANCELED(c@/!)")
 	      (sequence "OPEN(O)" "|" "CLOSED(C)"))))
 
 ;; Manage the global tag list
 (setq org-tag-alist '(("PROJECT" . ?p)))
-		      
+
 
 (setq org-todo-keyword-faces (quote (("TODO" :foreground "red" :weight bold)
  ("NEXT" :foreground "blue" :weight bold)
@@ -191,9 +173,9 @@
 
 ; Set up the summary to use.
 (defun org-summary-todo (n-done n-not-done)
-  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  "Switch entry to DONE when all sub-entries are done, to TODO otherwise."
 ; turn off logging
-  (let (org-log-done org-log-states) 
+  (let (org-log-done org-log-states)
     (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
 
 ;; Add some hooks
@@ -201,6 +183,16 @@
 
 ;; Turn off task priorities
 (setq org-enable-priority-commands nil)
+
+
+;;;;;
+;; ORG OUTLOOK
+;;;;;
+
+;; Only use this on windows
+;; (if (eq window-system 'w32)
+;;     (require 'org-outlook))
+
 
 ;;;;;
 ;; ORG CAPTURE
@@ -218,18 +210,19 @@
 	 (file "~/Documents/Org/Refile.org")
 	 "* %?                                                                            :NOTE:\n  %U\n  %a\n  :CLOCK:\n  :END:")
 	("w" "Weekly Report" entry
-	 (file+headline "~/Documents/Org/WeeklyReports.org" "2011")
+	 (file+headline "~/Documents/Org/WeeklyReports.org" "2012")
 	 "* %(bnb/workweek-string) \n %? \n %i")
-	))
+	("o" "org-outlook" entry
+	 (file "~/Documents/Org/Refile.org")
+	 "* TODO EMAIL: %:title (%:sender)
+  %c
+
+  %?" :clock-in t :clock-resume t)))
 
 ;;;;;
 ;; ORG REFILE
 ;;;;;
 
-(defun bnb/refile-to-personal-files ()
-  (interactive)
-  (if (file-exists-p "e:/org/Personal.org")
-      "e:/org/Personal.org"))
 
 ;; Cache the targets for refiling.  This is the best option for me as
 ;; I am usually refiling into separate files at a Tasks headline.
@@ -237,10 +230,9 @@
 (setq org-refile-use-cache t)
 ;; Allow refiling into any of the agenda files up to three levels
 ;; deep.  Also allow refiling in the same file up to 5 levels deep.
-(setq org-refile-targets (quote 
+(setq org-refile-targets (quote
 			  ((org-agenda-files :maxlevel . 3)
-			   (nil :maxlevel . 5)
-			   (bnb/refile-to-personal-files :maxlevel . 2))))
+			   (nil :maxlevel . 5))))
 ;; Filenames *must* be first for refiling.
 (setq org-refile-use-outline-path 'file)
 
@@ -260,9 +252,9 @@
 (setq org-agenda-clockreport-parameter-plist (quote (:link nil :maxlevel 2 :fileskip0 t)))
 
 ;; Only show a time grid on today if there is a scheduled task.
-(setq org-agenda-time-grid 
-      (quote ((daily today require-timed) 
-	      "----------------" 
+(setq org-agenda-time-grid
+      (quote ((daily today require-timed)
+	      "----------------"
 	      (800 1000 1200 1400 1600 1800 2000))))
 
 ;; Setup the various Agenda views
@@ -338,8 +330,8 @@
                 (org-agenda-overriding-header "Set default clocking task with C-u C-u I"))))))
 
 ;; Define Stuck projects for the Agenda view
-(setq org-stuck-projects 
-      (quote 
+(setq org-stuck-projects
+      (quote
        ("+LEVEL=2/-DONE" ("TODO" "NEXT" "NEXTACTION") ("REFERENCE" "ARCHIVE") "")))
 
 ;;;;;
@@ -351,6 +343,23 @@
 
 ;; Convert any LaTeX fragments to images for export.
 (setq org-export-with-LaTeX-fragments "dvipng")
+
+;; Minted (for latex) settings
+(setq org-export-latex-listings 'minted)
+(setq org-export-latex-custom-lang-environments
+      '(
+	(emacs-lisp "common-lispcode")
+	))
+(setq org-export-latex-minted-options
+      '(
+	))
+(setq org-latex-to-pdf-process
+      '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+	"xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+	"xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
+;; Remove HTML postamble
+(setq org-export-html-postamble nil)
 
 ;;;;;
 ;; ORG LOG
@@ -374,7 +383,8 @@
 (setq org-clock-in-resume t)
 
 ;; Change state to NEXT when clocking in a TODO item
-(setq org-clock-in-switch-to-state (quote bnb/clock-in-to-next))
+;;(setq org-clock-in-switch-to-state (quote bnb/clock-in-to-next))
+;;(setq org-clock-in-switch-to-state "NEXT")
 ;; Clock into the CLOCK drawer
 (setq org-clock-into-drawer "CLOCK")
 ;; Remove any 0 time clocks.
@@ -388,55 +398,91 @@
 ;; Include the clocking task in a clock report
 (setq org-clock-report-include-clocking-task t)
 
-;; Clock into default task or show a list of possible default tasks
-(defun bnb/clock-in ()
-  (interactive)
-  (if (marker-buffer org-clock-default-task)
-      (unless (org-clock-is-active)
-	(bnb/clock-in-default-task))
-    (org-agenda nil "c")))
+;; ;; Clock into default task or show a list of possible default tasks
+;; (defun bnb/clock-in ()
+;;   (interactive)
+;;   (if (marker-buffer org-clock-default-task)
+;;       (unless (org-clock-is-active)
+;; 	(bnb/clock-in-default-task))
+;;     (org-agenda nil "c")))
 
-;; Clock out if clock is active
-(defun bnb/clock-out ()
-  (interactive)
-  (when (org-clock-is-active)
-    (org-clock-out)))
+;; ;; Clock out if clock is active
+;; (defun bnb/clock-out ()
+;;   (interactive)
+;;   (when (org-clock-is-active)
+;;     (org-clock-out)))
 
-;; Clock into the default task
-(defun bnb/clock-in-default-task ()
-  (save-excursion
-    (org-with-point-at org-clock-default-task
-      (org-clock-in))))
+;; ;; Clock into the default task
+;; (defun bnb/clock-in-default-task ()
+;;   (save-excursion
+;;     (org-with-point-at org-clock-default-task
+;;       (org-clock-in))))
 
-;; Comment inline
-(defun bnb/clock-in-to-next (kw)
-  "Switch task from TODO to NEXT when clocking in.
-Skips remember tasks and tasks with subtasks"
-  (if (and (string-equal kw "TODO")
-	   (not (and (boundp 'org-capture-mode) org-capture-mode)))
-      (let ((subtree-end (save-excursion (org-end-of-subtree t)))
-	    (has-subtask nil))
-	(save-excursion
-	  (forward-line 1)
-	  (while (and (not has-subtask)
-		      (< (point) subtree-end)
-		      (re-search-forward "^\*+ " subtree-end t))
-	    (when (member (org-get-todo-state) org-not-done-keywords)
-	      (setq has-subtask t))))
-	(when (not has-subtask)
-	  "NEXT"))))
+;; ;; Comment inline
+;; (defun bnb/clock-in-to-next (kw)
+;;   "Switch task from TODO to NEXT when clocking in.
+;; Skips remember tasks and tasks with subtasks"
+;;   (if (and (string-equal kw "TODO")
+;; 	   (not (and (boundp 'org-capture-mode) org-capture-mode)))
+;;       (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+;; 	    (has-subtask nil))
+;; 	(save-excursion
+;; 	  (forward-line 1)
+;; 	  (while (and (not has-subtask)
+;; 		      (< (point) subtree-end)
+;; 		      (re-search-forward "^\*+ " subtree-end t))
+;; 	    (when (member (org-get-todo-state) org-not-done-keywords)
+;; 	      (setq has-subtask t))))
+;; 	(when (not has-subtask)
+;; 	  "NEXT"))))
 
 (setq org-time-stamp-rounding-minutes (quote (1 15)))
 
 ;; Set default column view headings: Task Effort Clock_Summary
-(setq org-columns-default-format 
+(setq org-columns-default-format
       "%80ITEM(Task) %5Score{+} %10Effort(Effort){:} %10CLOCKSUM")
 ;; Effort_ALL is set below in the ORG HABIT section.
 
 ;;;;;
 ;; ORG MODULES
 ;;;;;
-(setq org-modules (quote (org-bbdb org-bibtex org-crypt org-gnus org-id org-info org-jsinfo org-habit org-inlinetask org-irc org-protocol org-w3m org-bookmark org-crypt org-calc)))
+(setq org-modules
+      (quote
+       (org-bbdb
+	org-bibtex
+	org-crypt
+	org-gnus
+	org-id
+	org-info
+	org-jsinfo
+	org-habit
+	org-inlinetask
+	org-irc
+	org-protocol
+	org-w3m
+	org-bookmark
+	org-calc)))
+
+;;;;;
+;; ORG BABEL
+;;;;;
+
+;; Initialize the following languages by default
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (clojure . t)
+   (ditaa . t)
+   (dot . t)
+   (latex . t)
+   (python . t)
+   (perl . t)
+   (R . t)
+   (C . t)
+   (sqlite . t)))
+
+
+(setq org-babel-R-command "C:/Progra~1/R/R-2.15.1/bin/R --slave --no-save")
 
 ;;;;;
 ;; ORG CRYPT
@@ -454,7 +500,7 @@ Skips remember tasks and tasks with subtasks"
 ;; ORG HABIT
 ;;;;;
 (setq org-global-properties (quote (("STYLE_ALL" . "habit")
-				    ("Effort_ALL" . "0:10 0:30 1:00 2:00 3:00 4:00 8:00 12:00 16:00 20:00")
+				    ("Effort_ALL" . "0:10 0:30 1:00 2:00 3:00 4:00")
 				    ("Score_ALL" . "10 5 2 1"))))
 (setq org-habit-graph-column 50)
 
@@ -486,21 +532,21 @@ and store the file path as an org link.  Also pushes the URL to the `kill-ring'.
 ;;;;;
 ;; ORG IIMAGE
 ;;;;;
+;; TODO -- are these steps still necessary?
+;; (require 'iimage)
+;; ;; Fix the links to like windows paths.
+;; (add-to-list 'iimage-mode-image-regex-alist
+;; 	     (cons (concat "\\[\\[file:[a-zA-Z]:?\\(~?"
+;; 			   (concat "[-+./_0-9a-zA-Z%]+\\."
+;; 				   (regexp-opt (nconc (mapcar #'upcase
+;; 							      image-file-name-extensions)
+;; 						      image-file-name-extensions)
+;; 					       t))
+;;  			   "\\)\\]")  1))
 
-(require 'iimage)
-;; Fix the links to like windows paths.
-(add-to-list 'iimage-mode-image-regex-alist
-	     (cons (concat "\\[\\[file:[a-zA-Z]:?\\(~?" 
-			   (concat "[-+./_0-9a-zA-Z%]+\\."
-				   (regexp-opt (nconc (mapcar #'upcase
-							      image-file-name-extensions)
-						      image-file-name-extensions)
-					       t))
- 			   "\\)\\]")  1))
-
-(defadvice iimage-locate-file (before bnb/reinsert-spaces activate)
-  "Change %20 in the filename back into proper spaces"
-  (ad-set-arg 0 (replace-regexp-in-string "%20" " " (ad-get-arg 0))))
+;; (defadvice iimage-locate-file (before bnb/reinsert-spaces activate)
+;;   "Change %20 in the filename back into proper spaces"
+;;   (ad-set-arg 0 (replace-regexp-in-string "%20" " " (ad-get-arg 0))))
 
 
 ;;;;;
@@ -508,7 +554,7 @@ and store the file path as an org link.  Also pushes the URL to the `kill-ring'.
 ;;;;;
 (require 'org-feed)
 
-;; Setup feeds for ReQall, Reader and Foxmarks.
+;; Setup feeds for ReQall
 (setq org-feed-alist
       '(("ReQall"
          "http://www.reqall.com/user/feeds/rss/ef1f7fd093ca0a7d98ba5758d64b00775c47ccf3"
@@ -523,32 +569,71 @@ and store the file path as an org link.  Also pushes the URL to the `kill-ring'.
           '(("no_proxy" . ".*")) ))
     (org-feed-update-all)))
 
-    
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Org-publish for bnbeckwith.com
-(setq org-publish-project-alist
-      '(("org-bnb"  :base-directory "~/bnbeckwith.com/org/"
-	            :base-extension "org"
-		    :publishing-directory "~/bnbeckwith.com/jekyll"
-		    :recursive t
-		    :publishing-function org-publish-org-to-html
-		    :headline-levels 4
-		    :html-extension "html"
-		    :body-only t)
-	("static-bnb" :base-directory "~/bnbeckwith.com/org"
-	              :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php"
-		      :publishing-directory "~/bnbeckwith.com/jekyll"
-		      :recursive: t
-		      :publishing-function org-publish-attachment)
-	("bnb" :components ("org-bnb" "static-bnb"))))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Org-velocity
+;; ;; Org-publish for bnbeckwith.com
+;; (setq org-publish-project-alist
+;;       '(("org-bnb"  :base-directory "~/Documents/Personal/Projects/bnbeckwith.com/org"
+;; 	            :base-extension "org"
+;; 		    :publishing-directory "~/Documents/Personal/Projects/bnbeckwith.com/site"
+;; 		    :recursive t
+;; 		    :publishing-function org-publish-org-to-html
+;; 		    :headline-levels 4
+;; 		    :html-extension "html"
+;; 		    :style "
+;; <link rel=\"canonical\" href=\"http://sandbox.bnbeckwith.com/\" />
 
-(require 'org-velocity)
+;;   <!-- /ht Andy Clarke - http://front.ie/lkCwyf -->
+;;   <meta http-equiv=\"cleartype\" content=\"on\" />
+;;   <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\" />
+;;   <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
 
-(setq org-velocity-bucket "~/Documents/Org/Notes.org")
-(global-set-key (kbd "C-c v") 'org-velocity-read)
+;;   <link rel=\"shortcut icon\" href=\"/assets/ico/favicon.ico\" />
+;;   <link rel=\"apple-touch-icon\" href=\"/assets/ico/apple-touch-icon.png\" />
+
+;;   <!-- /ht Jeremy Keith - http://front.ie/mLXiaS -->
+;;   <link rel=\"stylesheet\" href=\"/assets/css/global.css\" media=\"all\" />
+;;   <link rel=\"stylesheet\" href=\"/assets/css/custom.css\" type=\"text/css\" media=\"screen\" />
+;;   <link rel=\"stylesheet\" href=\"/assets/css/layout.css\" media=\"all and (min-width: 33.236em)\" \>
+;;   <!-- 30em + (1.618em * 2) = 33.236em / Eliminates potential of horizontal scrolling in most cases -->
+
+;;   <!--[if (lt IE 9) & (!IEMobile)]>
+;;       <link rel=\"stylesheet\" href=\"/assets/css/layout.css\" media=\"all\">
+;;       <![endif]-->
+
+;;   <script src=\"/assets/js/libs/modernizr-1.7.min.js\"></script>
+;;   <link href=\"\" rel=\"alternate\" title=\"\" type=\"application/atom+xml\" />
+;; "
+;; 		    :html-preamble "</div>
+;; <div id=\"container\" class=\"cf\">
+;;     <div id=\"top\">
+;;   <header>
+;;     <h1> <span id=\"title\">bnbeckwith</span> </h1>
+;;     <ul id=\"topmenu\">
+;;       <li><a href=\"/index.html\">/Blog</a></li>
+;;       <li><a href=\"/categories.html\">/Categories</a></li>
+;;       <li><a href=\"/code.html\">/Code</a></li>
+;;       <li><a href=\"/about.html\">/About</a></li>
+;;       <li>
+;; 	<form method=\"GET\" action=\"https://duckduckgo.com/\" onSubmit=\"document.getElementById('sitesearch').value+=' site:bnbeckwith.com';\">
+;; 	  <input name=\"q\" type=\"text\" placeholder=\"/Search\" id=\"sitesearch\" />
+;;       </form></li>
+;;     </ul>
+;;   </header>
+;; </div>
+;;   <div id=\"main\" role=\"main\">
+;;    <div id=\"ignore\">
+;; "
+;; 		    :html-postamble
+;; "</div></div><footer>&copy; Benjamin Beckwith 2009-2012</footer></div>"
+;; 		    :auto-sitemap t
+;; 		    :style-include-default nil
+;; 		    :style-include-scripts nil)
+;; 	("static-bnb" :base-directory "~/Documents/Personal/Projects/bnbeckwith.com/org"
+;; 	              :base-extension "ico\\|css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php"
+;; 		      :publishing-directory "~/Documents/Personal/Projects/bnbeckwith.com/site"
+;; 		      :recursive: t
+;; 		      :publishing-function org-publish-attachment)
+;; 	("bnb" :components ("org-bnb" "static-bnb"))))
 
 (provide 'org-config)
