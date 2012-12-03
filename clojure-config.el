ab55826efc7616cebb309252104812b9c77fd96a
Benjamin Beckwith <benjamin.beckwith@intel.com>
2012-12-03 16:40:20 -0500

    Updated path and autoload for clojure

--- clojure-config.el
+++ clojure-config.el
@@ -2,8 +2,11 @@
 
 (add-to-list 'load-path (list (expand-file-name
 			       (concat bnb-elisp-dir "clojure-mode"))))
+(add-to-list 'exec-path "C:/Users/bnbeckwi/bin/")
 
-(require 'clojure-mode)
+(autoload 'clojure-mode "clojure-mode" "Paren paradise" t)
+(add-to-list 'auto-mode-alist
+	     '(".clj" . clojure-mode))
 (provide 'clojure-config)
 
 
