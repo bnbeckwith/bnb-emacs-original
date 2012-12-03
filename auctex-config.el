ea1037858dc663f161846a4c07694eca2b362214
Benjamin Beckwith <benjamin.beckwith@intel.com>
2012-12-03 16:36:35 -0500

    Updated auctex-config for pdf-latex and flyspell mode

--- auctex-config.el
+++ auctex-config.el
@@ -1,8 +1,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; AucTeX mode
 ;; Commented for faster load-times
-(load "auctex.el" t)
-(load "preview-latex.el" nil t t)
+
+;; (load "auctex.el" t)
+;; (load "preview-latex.el" nil t t)
 (when (eq system-type 'windows-nt)
   (require 'tex-mik))
 
@@ -24,11 +25,26 @@
 (setq TeX-parse-self t)
 
 ;; Extra Hooks
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
 (add-hook 'LaTeX-mode-hook 'orgtbl-mode)
 
+;; PDFLaTeX
+(setq TeX-PDF-mode t)
+(setq TeX-engine 'pdflatex)
+
 ;; Ask for master file
-(setq-default TeX-master nil)
+(setq-default TeX-master t)
+
+(setq TeX-output-view-style
+      (quote
+       (("^pdf$" "." "evince -f %o")
+        ("^html?$" "." "start %o"))))
+
+(setq TeX-engine 'xetex)
+
+;;("XeLaTeX" "%`xelatex %' %t" TeX-run-TeX nil (latex-mode doctex-mode) :help "Run XeLaTeX")
+
 
 (provide 'auctex-config)
